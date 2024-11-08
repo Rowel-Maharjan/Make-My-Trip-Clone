50e7ba45ce110b9bccf82c1f1d809f870decb215
Rowel Maharjan <rowelmhj@gmail.com>
2024-11-08 13:40:55 +0545

    Layout added

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import ErrorPage from './components/ErrorPage'
 import Holidays from './components/Holidays'
 import Homestay from './components/Homestay'
 import Hotels from './components/Hotels'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/hotels' element={<Hotels />}  ></Route>
-        <Route path='/holidays' element={<Holidays />} ></Route>
-        <Route path='/homestays' element={<Homestay />} ></Route>
-        <Route path='/bus' element={<Bus />} ></Route>
-        <Route path='/cabs' element={<Cabs />} ></Route>
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Hotels />}></Route>
+          <Route path='hotels' element={<Hotels />}></Route>
+          <Route path='holidays' element={<Holidays />} ></Route>
+          <Route path='homestays' element={<Homestay />} ></Route>
+          <Route path='bus' element={<Bus />} ></Route>
+          <Route path='cabs' element={<Cabs />} ></Route>
+        </Route>
         <Route path='*' element={<ErrorPage />} />
       </Routes>
     </>
