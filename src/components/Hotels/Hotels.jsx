14f46eb89bdbb578f3798666f251da279433f60c
Rocky Suwal <rockysuwal54@gmail.com>
2024-11-18 14:35:19 +0545

    Luxe Container Created

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,70 +1,176 @@
-
-import React, { useState } from 'react'
-import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
-import { Label } from '../ui/label'
-import { hotelRoomsInfo } from '../config'
-import Form from '../form'
-import { hotelRoomValue } from '../config'
-import { hotelGroupDeals } from '../config'
+import React, { useState } from "react";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Label } from "../ui/label";
+import { hotelRoomsInfo } from "../config";
+import Form from "../form";
+import { hotelRoomValue } from "../config";
+import { hotelGroupDeals } from "../config";
+import "../../GradientText.css";
+import background from "../../assets/Luxebackground.jpg";
+import image1 from "../../assets/luxeproperties.png";
+import image2 from "../../assets/Luxevilla.jpg";
+import image3 from "../../assets/Luxeinternational.jpg";
 
 const Hotels = () => {
-  const [hotelActiveTab, setHotelActiveTab] = useState("Upto 4 Rooms")
+  const [hotelActiveTab, setHotelActiveTab] = useState("Upto 4 Rooms");
   return (
     <>
-      <div className='w-[1200px] relative bg-white py-16 px-5 -top-12 rounded-xl mb-11 shadow-2xl'>
+      <div className="w-[1200px] relative bg-white py-16 px-5 -top-12 rounded-xl mb-11 shadow-2xl">
         {/* Tabs  */}
-        <div className='flex gap-10'>
+        <div className="flex gap-10">
           <RadioGroup className="flex gap-4 mb-2" defaultValue={hotelActiveTab}>
-            {
-              hotelRoomsInfo.map(roominfo =>
-                <div key={roominfo.id} onClick={() => { setHotelActiveTab(roominfo.value) }} className={`flex cursor-pointer items-center p-1 ${roominfo.value === hotelActiveTab ? 'bg-[#eaf5ff] rounded-full' : ''} `}>
-                  <RadioGroupItem value={roominfo.value} id={roominfo.id} />
-                  <Label className={`cursor-pointer pl-2 font-normal text-sm text-[#4a4a4a] ${roominfo.value === hotelActiveTab ? 'font-bold text-black' : ''}`} htmlFor={roominfo.id}>{roominfo.value}</Label>
-                </div>
-              )
-            }
+            {hotelRoomsInfo.map((roominfo) => (
+              <div
+                key={roominfo.id}
+                onClick={() => {
+                  setHotelActiveTab(roominfo.value);
+                }}
+                className={`flex cursor-pointer items-center p-1 ${
+                  roominfo.value === hotelActiveTab
+                    ? "bg-[#eaf5ff] rounded-full"
+                    : ""
+                } `}
+              >
+                <RadioGroupItem value={roominfo.value} id={roominfo.id} />
+                <Label
+                  className={`cursor-pointer pl-2 font-normal text-sm text-[#4a4a4a] ${
+                    roominfo.value === hotelActiveTab
+                      ? "font-bold text-black"
+                      : ""
+                  }`}
+                  htmlFor={roominfo.id}
+                >
+                  {roominfo.value}
+                </Label>
+              </div>
+            ))}
           </RadioGroup>
 
-          <div className='flex gap-1'>
-            <span className='font-semibold'>Book Domestic and International Property Online. To list your property</span>
-            <span className='text-blue-700 font-bold cursor-pointer'>Click Here</span>
+          <div className="flex gap-1">
+            <span className="font-semibold">
+              Book Domestic and International Property Online. To list your
+              property
+            </span>
+            <span className="text-blue-700 font-bold cursor-pointer">
+              Click Here
+            </span>
           </div>
-
         </div>
-
 
         {/* Content Box  */}
         {hotelActiveTab === "Upto 4 Rooms" && (
-          <div className='w-full border rounded-lg mb-5 flex border-[#e7e7e7]' >
-            {hotelRoomValue.map(roomValue => <Form key={roomValue.title} formValue={roomValue} />)}
+          <div className="w-full border rounded-lg mb-5 flex border-[#e7e7e7]">
+            {hotelRoomValue.map((roomValue) => (
+              <Form key={roomValue.title} formValue={roomValue} />
+            ))}
           </div>
         )}
 
         {hotelActiveTab === "Group Deals" && (
-          <div className='w-full border rounded-lg mb-5 flex border-[#e7e7e7]'>
-            {hotelGroupDeals.map(roomValue => <Form key={roomValue.title} formValue={roomValue} />)}
+          <div className="w-full border rounded-lg mb-5 flex border-[#e7e7e7]">
+            {hotelGroupDeals.map((roomValue) => (
+              <Form key={roomValue.title} formValue={roomValue} />
+            ))}
           </div>
         )}
 
-        <div className='text-sm text-[#4a4a4a] flex justify-center items-center gap-3'>
+        <div className="text-sm text-[#4a4a4a] flex justify-center items-center gap-3">
           <span>Trending Searches:</span>
-          <div className='p-1 rounded-sm cursor-pointer bg-[#f2f2f2]'>Mumbai, India</div>
-          <div className='p-1 rounded-sm cursor-pointer bg-[#f2f2f2]'>London, United Kingdom</div>
-          <div className='p-1 rounded-sm cursor-pointer bg-[#f2f2f2]'>Bangkok, Thailand</div>
+          <div className="p-1 rounded-sm cursor-pointer bg-[#f2f2f2]">
+            Mumbai, India
+          </div>
+          <div className="p-1 rounded-sm cursor-pointer bg-[#f2f2f2]">
+            London, United Kingdom
+          </div>
+          <div className="p-1 rounded-sm cursor-pointer bg-[#f2f2f2]">
+            Bangkok, Thailand
+          </div>
         </div>
-        <p className='flex justify-center relative'>
-          {hotelActiveTab === "Upto 4 Rooms" &&
-            <button className='absolute -bottom-16 px-5 py-2 -mb-5 inline-block bg-gradient-to-r min-w-[216px] from-[#53b2fe] to-[#065af3] rounded-full text-white text-2xl font-bold '>SEARCH</button>}
-          {hotelActiveTab === "Group Deals" &&
-            <button className='absolute -bottom-16 px-5 py-2 -mb-5 inline-block bg-gradient-to-r min-w-[216px] from-[#53b2fe] to-[#065af3] rounded-full text-white text-2xl font-bold '>GET ME BEST PRICES</button>}
+        <p className="flex justify-center relative">
+          {hotelActiveTab === "Upto 4 Rooms" && (
+            <button className="absolute -bottom-16 px-5 py-2 -mb-5 inline-block bg-gradient-to-r min-w-[216px] from-[#53b2fe] to-[#065af3] rounded-full text-white text-2xl font-bold ">
+              SEARCH
+            </button>
+          )}
+          {hotelActiveTab === "Group Deals" && (
+            <button className="absolute -bottom-16 px-5 py-2 -mb-5 inline-block bg-gradient-to-r min-w-[216px] from-[#53b2fe] to-[#065af3] rounded-full text-white text-2xl font-bold ">
+              GET ME BEST PRICES
+            </button>
+          )}
         </p>
       </div>
-      <div className='w-[1200px] h-80  relative bg-white py-16 px-5 -top-12 rounded-xl shadow-2xl'>
-        From Here
 
+      {/* Luxe Container */}
+      <div
+        className="w-[1200px] relative bg-white py-[34px] pr-[30px] pl-[40px] -top-12 rounded-xl mb-11 shadow-2xl h-auto flex"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* Text Section */}
+        <div className="flex px-2 w-[32%] mt-6">
+          <div>
+            <h3 className="font-bold text-sm">INTRODUCING</h3>
+            <h2 className="gradient-text font-black leading-[45px]">
+              MMT Luxe Selections
+            </h2>
+            <p className="font-bold">
+              Escape to the epitome of luxury, packed with signature amenities
+              and services
+            </p>
+            <button className="mt-3 px-7 py-2 font-bold bg-gradient-to-r from-[#edd287] to-[#b8860b] rounded-full">
+              Learn More
+            </button>
+          </div>
+        </div>
+
+        {/* Image Section */}
+        <div className="grid grid-cols-3 gap-3 w-[70%] ml-6">
+          <div className="w-[233px] h-auto bg-gray-100 rounded-md shadow-lg">
+            <img
+              src={image1}
+              alt="Luxury Property 1"
+              className="w-full h-[170px] object-cover rounded-t-md"
+            />
+            <div className="px-[16px] py-[10px] leading-[20px]">
+              <h1 className="font-black text-[20px]">
+                Luxe properties in India
+              </h1>
+              <p className="text-[#4a4a4a]">
+                Explore by Luxary brands, themes and top picks
+              </p>
+            </div>
+          </div>
+          <div className="w-[233px] h-auto bg-gray-100 rounded-md shadow-lg">
+            <img
+              src={image2}
+              alt="Luxury Property 2"
+              className="w-full h-[170px] object-cover rounded-t-md"
+            />
+            <div className="px-[16px] py-[10px] leading-[20px]">
+              <h1 className="font-black text-[20px]">Luxe Villas</h1>
+              <p className="text-[#4a4a4a]">
+                Premium Villas with Superlative Experience
+              </p>
+            </div>
+          </div>
+          <div className="w-[233px] h-auto bg-gray-100 rounded-md shadow-lg">
+            <img
+              src={image3}
+              alt="Luxury Property 3"
+              className="w-full h-[170px] object-cover rounded-t-md"
+            />
+            <div className="px-[16px] py-[10px] leading-[20px]">
+              <h1 className="font-black text-[20px]">Luxe International</h1>
+              <p className="text-[#4a4a4a]">Dubai, Maldives, Thailand & More</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Hotels
+export default Hotels;
