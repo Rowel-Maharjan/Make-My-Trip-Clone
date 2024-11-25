721efcfca9594c7e0ace0bfa7297e6fc1c0188fa
Rowel Maharjan <rowelmhj@gmail.com>
2024-11-25 15:38:46 +0545

    Footer Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    const Icon = ({ type }) => {
+        return <span className={`icon ${type}`} />;
+    };
+    return (
+        <div className='bg-black text-white pt-12 px-0 pb-14'>
+            <div className='w-[1200px] flex mx-auto justify-between'>
+                <div className='flex items-center gap-8'>
+                    <Icon type="instagramIcon" />
+                    <Icon type="twitterIcon" />
+                    <Icon type="linkedinIcon" />
+                    <Icon type="facebookIcon" />
+                </div>
+                <div className='flex flex-col'>
+                    <div className='font-bold text-base'>&copy; 2024 MAKEMYTRIP PVT.LTD.</div>
+                    <div className='flex justify-end gap-1'>
+                        <span className='font-normal text-sm'>Country</span>
+                        <span className='font-black text-sm cursor-pointer'>Nepal India Maldives</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Header from './Header'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = () => {
     return (<>
@@ -15,6 +16,7 @@ const Layout = () => {
                 </div>
                 <Outlet />
             </div>
+            <Footer />
         </div>
     </>
     )
